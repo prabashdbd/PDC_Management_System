bad0c8de39f1bd91770b23ca8b2ed2e24fac96ff
prabashdbd <prabash_dbd@yahoo.com>
2019-02-20 14:47:49 +0530

    profile files

---
+++ project1/app/imgfile.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+
+class imgfile extends Model
+{
+    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'imgfiles';
+    protected $fillable = [
+    	
+        'img_name',
+        'img_path',
+    ];
+    
+    public function student()
+	{
+        return $this->belongsTo('App\student','student_id');
+        
+    }
+    
+}
