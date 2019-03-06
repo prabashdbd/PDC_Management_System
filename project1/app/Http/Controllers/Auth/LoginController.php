9353bde042873f7a2daf4ffceeb75c11a6b26e42
prabashdbd <prabash_dbd@yahoo.com>
2019-03-06 15:38:14 +0530

    login

--- project1/app/Http/Controllers/Auth/LoginController.php
+++ project1/app/Http/Controllers/Auth/LoginController.php
@@ -37,18 +37,16 @@ class LoginController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('guest')->except('logout');
+    // }
 
 
     public function validateUserEmail()
     {
-        $username = $_POST['email']; 
-        // return $username;
-        $count = DB::table('users')->select('username')->where('username', $username)->count();
-        // return response()->json($count);
+        $username = $_POST['email'];
+        $count = DB::table('users')->select('username')->where('username', $username)->count();        
         if($username !=null){
             if($count<=0){
                 echo "Email has not been registered";
@@ -62,15 +60,7 @@ class LoginController extends Controller
     public function validateUserPassword(Request $request)
     {
         $password = $_POST['password'];
-        $username = $_POST['email'];
-        // $user = User::where('username', '=', $username)->first();        
-        // $hash_password = Hash::make($password);
-        // $return = $user->password.','.$hash_password;
-        
-        // $return = $password.','.$hash_password;
-        // return response()->json($return);
-
-        // $count = User::Where('password',$password)->where('username',$username)->first();
+        $username = $_POST['email'];        
         $count = User::Where('password',$password)->where('username',$username)->count();        
         if($username !=null){            
             if($password != null){
@@ -89,70 +79,50 @@ class LoginController extends Controller
     }
 
 
-    // public function userLogin(Request $request)
-    // {   
-    //     $password = $_POST['password'];
-    //     $username = $_POST['email'];
-    //     if($username !=null){            
-    //         if($password != null){                
-    //             $user = User::where('username', '=', $username)->first();
-    //             // return response()->json($username);
-    //             $count = User::Where('password',$password)->where('username',$username)->count();
-    //             if($count<=0){
-    //                 echo "Credentials don't match";
-    //             }
-    //             else{
-    //                 $user_id = $user->id;
-    //                 $student_id=$user->student_id;
-    //                 $company_id=$user->company_id;
-    //                 $role_id=$user->role_id;
-    //                 if($role_id==3 || $role_id==4){ 
-    //                     if($student_id!=null && $company_id==null){
-    //                         // echo "This is a student";
-    //                         return view('layouts.adminlte',compact('student_id','role_id'));
-    //                     }
-    //                     else{
-    //                         // return view('layouts.adminlte',compact('company_id','role_id'));
+    
 
-    //                     }
-                        
-    //                 }elseif($role_id==2){
-    //                     // return view('layouts.adminlte',compact('id','role_id'));
+    // public function userLoginTest(Request $request){
 
-    //                 }else{
-    //                     // return view('layouts.adminlte',compact('id','role_id'));
-    //                 }
+    //     $username = $request->email;
+    //     $password = $request->password;
 
-    //             }
-    //         }
-    //         else{
-    //             echo "Password field is required";
-    //         }          
+    //     // Auth::login(User::Where('username','=',$username)->Where('password','=',$password)->first());
+    //     // return view('layouts.adminlte');
+    //     //return $username;
+
+    //     if($user = User::Where('username','=',$username)->Where('password','=',$password)->first()){
+    //         //return Auth::user()->id;
+    //         $userid = $user->student_id;
+    //         //return $userid;
+    //         return view('layouts.adminlte',compact('userid'));
     //     }
-    //     else{
-    //         echo "Email field is required";
-    //     }
+        
+    //     return view('auth.login1');
 
     // }
 
-    public function userLoginTest(Request $request){
 
-        $username = $request->email;
-        $password = $request->password;
 
-        // Auth::login(User::Where('username','=',$username)->Where('password','=',$password)->first());
-        // return view('layouts.adminlte');
-        //return $username;
+    public function authenticate(Request $request)
+    {
+        $this->validate($request,[
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+        $user = User::where('username', $request->email)
+            ->where('password', $request->password)
+            ->first();
 
-        if($user = User::Where('username','=',$username)->Where('password','=',$password)->first()){
-            //return Auth::user()->id;
-            $userid = $user->student_id;
-            //return $userid;
-            return view('layouts.adminlte',compact('userid'));
+        if($user) {
+            // Auth::loginUsingId($user->id);
+            // -- OR -- //
+            Auth::login($user,true);
+            return redirect()->route('dashboard');
+        } 
+        else
+        {
+            return redirect()->route('login_page');
         }
-        
-        return view('auth.login1');
-
     }
 
     
